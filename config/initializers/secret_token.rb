f1d51ce2ab3fa6b686dfcc866ae4ffb852ebdeac
diane.uwacu <diane.uwacu@eagles.oc.edu>
2013-01-21 22:28:43 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chp2App::Application.config.secret_token = 'd3815bc7f52d144616fcc191179fffa3f69bfb3e06c5a7b0d58d1d2387b2d2897552a8216c21f35aaa333afafc93c6ffb02765cebfca1afcee331dd4673a8397'
